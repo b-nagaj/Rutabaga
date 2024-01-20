e52eee8358d648bc3e9c6f72f7a29af9c055777e
b-nagaj <bryce.nagaj@gmail.com>
2024-01-20 14:02:51 -0500

    Add the Card component to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,36 @@
 
 import Section from "../components/Section/Section.tsx"
 import Intro from "../components/Intro/Intro.tsx"
+import { Card, CardProps } from "../components/Card/Card.tsx"
 
 const title = "Rutabaga Reading"
 const subtitle = "Keep all the metrics about your favorite books, all in one place. Rutabaga running is a platform where you can keep tallies on the books you've read and what you love about them."
 
+const contentRowValues: CardProps = {
+  titles: ['Test 1', 'Test 2', 'Test 3'],
+  images: ['https://i.imgur.com/MK3eW3Am.jpg', 'https://i.imgur.com/7vQD0fPs.jpg', 'https://i.imgur.com/1bX5QH6.jpg'],
+  imageAlts: ['Picture #1', 'Picture #2', 'Picture #3'],
+  descriptions: ['Description 1', 'Description 2', 'Description 3'],
+};
+
 function Home() {
   return (
-    <Section>
-      <Intro
-        title={title}
-        subtitle={subtitle}>
-      </Intro>
-    </Section>
+    <>
+      <Section>
+        <Intro
+          title={title}
+          subtitle={subtitle}>
+        </Intro>
+      </Section>
+      <Section className="content-row-background">
+        <Card 
+          titles={contentRowValues.titles}
+          images={contentRowValues.images}
+          imageAlts={contentRowValues.imageAlts}
+          descriptions={contentRowValues.descriptions}>
+        </Card>
+      </Section>
+    </>
   )
 }
 
