af5e3277675a31a6484a264eebd8a550500ecca5
b-nagaj <bryce.nagaj@gmail.com>
2024-02-06 21:24:55 -0500

    Add new images to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@
 
 import Section from "../components/Section/Section.tsx";
 import { Intro, IntroProps } from "../components/Intro/Intro.tsx";
+
 import {
   ContentRow,
   ContentRowProps,
@@ -16,11 +17,15 @@ const introContent: IntroProps = {
 const contentRowValues: ContentRowProps = {
   titles: ["Log A Book", "Add to Your Wishlist", "View & Manage Goals"],
   images: [
-    "https://i.imgur.com/MK3eW3Am.jpg",
-    "https://i.imgur.com/7vQD0fPs.jpg",
-    "https://i.imgur.com/1bX5QH6.jpg",
+    "../public/images/cards/logABook.jpeg", // https://www.freepik.com/free-vector/novelist-writing-concept-illustration_9148013.htm#page=3&query=cartoon%20author&position=15&from_view=search&track=ais&uuid=4d42551b-94ce-4d37-b4c0-c9d1786f88f1
+    "../public/images/cards/createAWishlist.jpg", // https://www.freepik.com/free-vector/hand-drawn-black-teacher-clipart-illustration_84406351.htm#query=cartoon%20author%20borderless&position=19&from_view=search&track=ais&uuid=1a19fdd9-66f6-4f38-81eb-216649b77f58
+    "../public/images/cards/viewAndManageGoals.jpg", // https://www.freepik.com/free-vector/business-people-success-concept_5727734.htm#page=2&query=cartoon%20goal%20achiever%20female&position=4&from_view=search&track=ais&uuid=eb735761-783f-4d35-b0c0-005516ebc24a
   ],
-  imageAlts: ["Log A Book", "Add to Your Wishlist", "View & Manage Goals"],
+  imageAlts: [
+    "A cartoon reader writing a review",
+    "A cartoon man holding a wishlist",
+    "A cartoon hiker accomplishing his goals",
+  ],
   descriptions: [
     "Put on the glasses of a literary illusionist. Leave your review on a book you've recently added to your repertoire",
     "Craft a personalized treasure map to your desires. Make a list of books you can't live without",
